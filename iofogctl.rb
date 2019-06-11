627714fe54df06eb0605ee35104ab4216039f853
SergeRadinovich <serge@edgeworx.io>
2019-06-11 15:17:24 +1200

    Add edgeworx download url

--- iofogctl.rb
+++ iofogctl.rb
@@ -1,8 +1,8 @@
 class Iofogctl < Formula
   desc "Command line tool for deploying and administering ioFog platforms"
   homepage "https://github.com/eclipse-iofog/iofogctl"
-  url ""
-  sha256 ""
+  url "http://edgeworx.io/downloads/iofogctl/iofogctl.tar.gz"
+  sha256 "a2a09d0a42911a889e16ef1959740b885dcff4085265767cdbca728338d99747"
   version "0.0.0"
 
   depends_on "curl"
