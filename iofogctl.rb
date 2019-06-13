2eca205542cedbc0c6a6a5b0102cab8f22124cf3
Serge Radinovich <serge@edgeworx.io>
2019-06-13 20:33:31 +0000

    Publish version 0.0.0-4ea44

--- iofogctl.rb
+++ iofogctl.rb
@@ -3,7 +3,7 @@ class Iofogctl < Formula
   homepage "https://github.com/eclipse-iofog/iofogctl"
   url "http://edgeworx.io/downloads/iofogctl/iofogctl.tar.gz"
   sha256 "e565b7bd2ea67fd09b73051b5f72c31580649ba85c92667afb8ee9577359d9ee"
-  version "0.0.0"
+  version "0.0.0-4ea44"
   devel do
     url "http://edgeworx.io/downloads/iofogctl/iofogctl-dev.tar.gz"
     sha256 ""
