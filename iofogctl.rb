81b4578224cadd5fd7002f85f103b377c5362124
Serge Radinovich <serge@edgeworx.io>
2019-06-13 08:44:38 +0000

    Update release checksum to c7c955e226217bdf1add5aa391f93657f7936f515eb009cf588100e265914383

--- iofogctl.rb
+++ iofogctl.rb
@@ -2,7 +2,7 @@ class Iofogctl < Formula
   desc "Command line tool for deploying and administering ioFog platforms"
   homepage "https://github.com/eclipse-iofog/iofogctl"
   url "http://edgeworx.io/downloads/iofogctl/iofogctl.tar.gz"
-  sha256 "$checksum"
+  sha256 "c7c955e226217bdf1add5aa391f93657f7936f515eb009cf588100e265914383"
   version "0.0.0"
   devel do
     url "http://edgeworx.io/downloads/iofogctl/iofogctl-dev.tar.gz"
