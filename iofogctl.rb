cf9d1eae0c7e09d2dea4b916478eaa9b94a5eb60
Serge Radinovich <serge@edgeworx.io>
2019-06-14 01:12:01 +0000

    Publish develop version 0.0.3-40673

--- iofogctl.rb
+++ iofogctl.rb
@@ -6,8 +6,8 @@ class Iofogctl < Formula
   version "0.0.3"
   devel do
     url "http://edgeworx.io/downloads/iofogctl/iofogctl-dev.tar.gz"
-    sha256 "a0a130d9a65267b5385e24e2838f6a485e0e549709a3f8003536106823432648"
-    version "0.0.3"
+    sha256 "b90c152033a95ab1f644072d7fc5085f475a68db17c984e1bf62ac3acc7eee0a"
+    version "0.0.3-40673"
   end
 
   depends_on "curl"
