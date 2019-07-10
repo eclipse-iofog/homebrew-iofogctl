aa8dbdb4f8f484ba445dfc437add43034ce4e932
SergeRadinovich <serge@edgeworx.io>
2019-07-11 09:21:56 +1200

    Update url directories for rel and dev

--- iofogctl.rb
+++ iofogctl.rb
@@ -1,11 +1,11 @@
 class Iofogctl < Formula
   desc "Command line tool for deploying and administering ioFog platforms"
   homepage "https://github.com/eclipse-iofog/iofogctl"
-  url "http://edgeworx.io/downloads/iofogctl/iofogctl.tar.gz"
+  url "http://edgeworx.io/downloads/iofogctl/rel/1.0.0.tar.gz"
   sha256 "bf64e98b5a121b70e2068b950f1edb943ff411766e33d5e0f4a6e5ebedd41287"
   version "1.0.0"
   devel do
-    url "http://edgeworx.io/downloads/iofogctl/iofogctl-dev.tar.gz"
+    url "http://edgeworx.io/downloads/iofogctl/dev/1.0.0-b2068.tar.gz"
     sha256 "95498781a8a39fa243550bfb40cdf216e438a4f2dc6bf14b4f72b54daf17ce63"
     version "1.0.0-b2068"
   end
