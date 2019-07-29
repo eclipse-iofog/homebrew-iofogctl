b25f5b7ace9f2ae4e040de5706857a5901ed6992
Serge Radinovich <serge@edgeworx.io>
2019-07-29 05:34:06 +0000

    Publish develop version 1.2.1-b2766

--- iofogctl.rb
+++ iofogctl.rb
@@ -5,9 +5,9 @@ class Iofogctl < Formula
   sha256 "1012143f642e530ba9ef1a948cb676875d71b77bdc73b48474358a2d04b43b1a"
   version "1.2.1"
   devel do
-    url "http://edgeworx.io/downloads/iofogctl/dev/1.2.1-b2765.tar.gz"
-    sha256 "94a835df0e99e9456f33c9ceef6f9596be2446108ac431d1b6d4fefe78b9c5dc"
-    version "1.2.1-b2765"
+    url "http://edgeworx.io/downloads/iofogctl/dev/1.2.1-b2766.tar.gz"
+    sha256 "5213b39a76237838f8b599211c65d733e6860dbdb60d963f8351d8659820dbc3"
+    version "1.2.1-b2766"
   end
 
   depends_on "curl"
