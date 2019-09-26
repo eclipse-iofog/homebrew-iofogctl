1928ca0783a9f9ce32b7c22b1d1a465c3c5c0a59
Serge Radinovich <serge@edgeworx.io>
2019-09-26 23:50:51 +0000

    Publish release version 1.3.0-rc3

--- iofogctl@1.3.rb
+++ iofogctl@1.3.rb
@@ -1,9 +1,9 @@
 class IofogctlAT13 < Formula
   desc "Command line tool for deploying and administering ioFog platforms"
   homepage "https://github.com/eclipse-iofog/iofogctl"
-  url "http://edgeworx.io/downloads/iofogctl/rel/<REL_VERSION>.tar.gz"
-  sha256 "<REL_SHA256>"
-  version "<REL_VERSION>"
+  url "http://edgeworx.io/downloads/iofogctl/rel/1.3.0-rc3.tar.gz"
+  sha256 "d09cb9019eb600704ca722e1c1a4220d382cffca3f4a7e564e64e6e157f6bf36"
+  version "1.3.0-rc3"
   devel do
     url "http://edgeworx.io/downloads/iofogctl/dev/1.3.0-rc3.tar.gz"
     sha256 "c5d3a80644aaa514db57d5094f5e7c8f2e11fdd113e52f2a85eed6b555211219"
