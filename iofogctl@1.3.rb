07eee7bba90d3b46e0e6b224ca921531510d92ba
Serge Radinovich <serge@edgeworx.io>
2019-11-24 21:27:37 +0000

    Publish develop version 1.3.0-dev

--- iofogctl@1.3.rb
+++ iofogctl@1.3.rb
@@ -6,7 +6,7 @@ class IofogctlAT13 < Formula
   version "1.3.0"
   devel do
     url "https://storage.cloud.google.com/iofogctl/osx/1.3/dev/iofogctl.tar.gz"
-    sha256 "96a1a1fdee28aa11c8e33a2435a431aba44be96e1c50edbca48c3ed446f8c0a0"
+    sha256 "18c2d79fb08e87bc2fd7435fdd957ac4e54b1d289683879fd5047aa9fbb266ca"
     version "1.3.0-dev"
   end
 
