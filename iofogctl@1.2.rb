52142502624d38d84e0debc87b9222bf27ed99d4
SergeRadinovich <serge@edgeworx.io>
2019-11-25 15:29:47 +1300

    Migrate artifacts to GCP

--- iofogctl@1.2.rb
+++ iofogctl@1.2.rb
@@ -1,11 +1,11 @@
 class IofogctlAT12 < Formula
   desc "Command line tool for deploying and administering ioFog platforms"
   homepage "https://github.com/eclipse-iofog/iofogctl"
-  url "http://edgeworx.io/downloads/iofogctl/rel/1.2.5.tar.gz"
+  url "https://storage.googleapis.com/iofogctl/osx/1.2/iofogctl.tar.gz"
   sha256 "22c2444f6a8ddf81332696e230c176b120883b382297f0b1a5dd8a93292c0cd3"
   version "1.2.5"
   devel do
-    url "http://edgeworx.io/downloads/iofogctl/dev/1.2.5.tar.gz"
+    url "https://storage.googleapis.com/iofogctl/osx/dev/1.2/iofogctl.tar.gz"
     sha256 "22c2444f6a8ddf81332696e230c176b120883b382297f0b1a5dd8a93292c0cd3"
     version "1.2.5"
   end
--- iofogctl@1.3.rb
+++ iofogctl@1.3.rb
@@ -1,7 +1,7 @@
 class IofogctlAT13 < Formula
   desc "Command line tool for deploying and administering ioFog platforms"
   homepage "https://github.com/eclipse-iofog/iofogctl"
-  url "http://edgeworx.io/downloads/iofogctl/rel/1.3.0.tar.gz"
+  url "https://storage.googleapis.com/iofogctl/osx/1.3/iofogctl.tar.gz"
   sha256 "c04f1b850992d3734c6e8611e40db85ba1a014c577432e71a34535d07d4c3e02"
   version "1.3.0"
   devel do
