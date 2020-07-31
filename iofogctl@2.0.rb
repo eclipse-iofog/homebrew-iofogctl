6af1c83d0e94156b735b83a185f81367f09008f1
Serge Radinovich <serge@edgeworx.io>
2020-07-31 02:07:44 +0000

    Publish develop version 2.0.0-dev

--- iofogctl@2.0.rb
+++ iofogctl@2.0.rb
@@ -6,7 +6,7 @@ class IofogctlAT20 < Formula
   version "2.0.0-rc3"
   devel do
     url "https://storage.googleapis.com/iofogctl/osx/dev/2.0/iofogctl.tar.gz"
-    sha256 "c2537b19f3312efe5d23f17ef5800711283566079cc80f75a06c3a58812cf3c9"
+    sha256 "891bac6223959f1783ab7c0e39242f8e6751128128da841b7108858bcd1881df"
     version "2.0.0-dev"
   end
 
