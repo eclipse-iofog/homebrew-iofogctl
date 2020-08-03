8d5a041a8e1170b6b4f3953f308fe3c58c25043a
SergeRadinovich <serge@edgeworx.io>
2020-08-03 13:26:54 +1200

    Remove devel block from iofogctl.rb

--- iofogctl.rb
+++ iofogctl.rb
@@ -4,11 +4,6 @@ class Iofogctl < Formula
   url "https://storage.googleapis.com/iofogctl/osx/2.0/iofogctl.tar.gz"
   sha256 "51cf31f37f8cca568c3710987fc380dbe55fcdf26273a1df78460d7c5e464acb"
   version "2.0.0-rc3"
-  devel do
-    url "https://storage.googleapis.com/iofogctl/osx/dev/2.0/iofogctl.tar.gz"
-    sha256 "7ae3167d5cddb74146361b445cd4eea2d671d6b9d4d0a5a6c3cd1cef9d1667d4"
-    version "2.0.0-rc3"
-  end
 
   depends_on "curl"
   depends_on "bash-completion"
