3149270a93cf6cf94231f63649ae88a56350f5be
Serge Radinovich <serge@edgeworx.io>
2021-01-14 01:07:31 +0000

    Publish develop version 2.0.3-dev

--- iofogctl@2.0-devel.rb
+++ iofogctl@2.0-devel.rb
@@ -2,8 +2,8 @@ class IofogctlAT20Devel < Formula
   desc "Command line tool for deploying and administering ioFog platforms"
   homepage "https://github.com/eclipse-iofog/iofogctl"
   url "https://storage.googleapis.com/iofogctl/osx/dev/2.0.3/iofogctl.tar.gz"
-  sha256 "c04a74bb1ba7bc5a6f605e152bfca500eff025c78e2b5f1d3bc2f4f22e5f7e65"
-  version "2.0.3-dev-b11419"
+  sha256 "a5587fbf4c22aec3653b6ddd54778abf7d32dc3be4766b06a3078b51698c5d4c"
+  version "2.0.3-dev-b11425"
 
   depends_on "curl"
   depends_on "bash-completion"
