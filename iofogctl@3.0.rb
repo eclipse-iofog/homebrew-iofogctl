454bba7cbf39183ebf34434c4393edf34fba41d7
Serge Radinovich <serge@edgeworx.io>
2021-03-25 01:25:25 +0000

    Publish release version 3.0.0-alpha1

--- iofogctl@3.0.rb
+++ iofogctl@3.0.rb
@@ -1,4 +1,4 @@
-class Iofogctl < Formula
+class IofogctlAT30Devel < Formula
   desc "Command line tool for deploying and administering ioFog platforms"
   homepage "https://github.com/eclipse-iofog/iofogctl"
   url "https://storage.googleapis.com/iofogctl/osx/3.0.0/iofogctl.tar.gz"
