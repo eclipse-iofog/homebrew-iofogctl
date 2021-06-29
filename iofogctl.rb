b764c2250f00520cd8f512b4c7207f37a370d7ce
Serge Radinovich <serge@edgeworx.io>
2021-06-29 23:01:11 +0000

    Publish release version 2.0.7

--- iofogctl.rb
+++ iofogctl.rb
@@ -1,9 +1,9 @@
 class Iofogctl < Formula
   desc "Command line tool for deploying and administering ioFog platforms"
   homepage "https://github.com/eclipse-iofog/iofogctl"
-  url "https://storage.googleapis.com/iofogctl/osx/3.0.0/iofogctl.tar.gz"
-  sha256 "a6e55a5af7a47b0d61556000f33ff2571aee927c49f4ee276af26e4556e5de9e"
-  version "3.0.0-alpha1"
+  url "https://storage.googleapis.com/iofogctl/osx/2.0.7/iofogctl.tar.gz"
+  sha256 "7c415d28ce1a1c0fba3c1eea3b918f609dfcf5371c87a964ec9bfc7a5d7cc36c"
+  version "2.0.7"
 
   depends_on "curl"
   depends_on "bash-completion"
--- iofogctl@2.0.rb
+++ iofogctl@2.0.rb
@@ -1,9 +1,9 @@
 class IofogctlAT20 < Formula
   desc "Command line tool for deploying and administering ioFog platforms"
   homepage "https://github.com/eclipse-iofog/iofogctl"
-  url "https://storage.googleapis.com/iofogctl/osx/2.0.5/iofogctl.tar.gz"
-  sha256 "424fa6fb7cc2874645a00062c8599d00e37fbe458b40f0bfe0a6bcf274c453e3"
-  version "2.0.5"
+  url "https://storage.googleapis.com/iofogctl/osx/2.0.7/iofogctl.tar.gz"
+  sha256 "7c415d28ce1a1c0fba3c1eea3b918f609dfcf5371c87a964ec9bfc7a5d7cc36c"
+  version "2.0.7"
 
   depends_on "curl"
   depends_on "bash-completion"
